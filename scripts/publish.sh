484d8c41c6e112afb47bb18be4ad5594de384c00
YOU54F <yousafn@gmail.com>
2019-06-02 22:56:05 +0100

    ci: npm key

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,6 @@
 
 VERSION=$(grep '\"version\"' package.json | grep -E -o "([0-9\.]+(-[a-z\.0-9]+)?)")
 echo "--> Releasing version ${VERSION}"
-echo "    Publishing jest-pact@${VERSION}..."
+echo "    Publishing cypress-slack-reporter@${VERSION}..."
 npm publish --tag latest
 echo "    done!"
