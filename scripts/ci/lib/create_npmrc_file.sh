9afbfa810fbe1cdfb9de62117882e5f38796b774
Yousaf Nabi <yousafn@gmail.com>
2022-04-07 20:10:22 +0100

    ci: update npmrc and trigger-release script

--- scripts/ci/lib/create_npmrc_file.sh
+++ scripts/ci/lib/create_npmrc_file.sh
@@ -7,10 +7,10 @@ require_env_var NODE_AUTH_TOKEN
 set +x #Don't echo the NPM key
 
 NPMRC_FILE=.npmrc
-echo  "@pact-foundation:registry=https://registry.npmjs.org/" > $NPMRC_FILE
+echo "YOU54F:registry=https://registry.npmjs.org/" > $NPMRC_FILE
 echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> $NPMRC_FILE
-echo "//registry.npmjs.org/:username=pact-foundation" >> $NPMRC_FILE
-echo "//registry.npmjs.org/:email=pact-foundation@googlegroups.com" >> $NPMRC_FILE
+echo "//registry.npmjs.org/:username=you54f" >> $NPMRC_FILE
+echo "//registry.npmjs.org/:email=yousafn@gmail.com" >> $NPMRC_FILE
 echo "//registry.npmjs.org/:always-auth=true" >> $NPMRC_FILE
 
 set -x
--- scripts/trigger-release.sh
+++ scripts/trigger-release.sh
@@ -6,17 +6,17 @@
 # Borrowed from Beth Skurrie's excellent script at
 # https://github.com/pact-foundation/pact-ruby/blob/master/script/trigger-release.sh
 
-: "${GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES:?Please set environment variable GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES}"
+: "${GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES:?Please set environment variable GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES}"
 
 repository_slug=$(git remote get-url $(git remote show) | cut -d':' -f2 | sed 's/\.git//')
 
 output=$(curl -L -v -X POST https://api.github.com/repos/${repository_slug}/dispatches \
       -H 'Accept: application/vnd.github.everest-preview+json' \
-      -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES" \
+      -H "Authorization: Bearer $GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES" \
       -d "{\"event_type\": \"release-triggered\"}" 2>&1)
 
 if  ! echo "${output}" | grep "HTTP\/2 204" > /dev/null; then
-  echo "$output" | sed  "s/${GITHUB_ACCESS_TOKEN_FOR_PF_RELEASES}/********/g"
+  echo "$output" | sed  "s/${GITHUB_ACCESS_TOKEN_FOR_YOU54F_RELEASES}/********/g"
   echo "Failed to trigger release"
   exit 1
 else
