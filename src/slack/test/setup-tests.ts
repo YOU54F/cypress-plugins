ccd6f66887b45fed2aeccd791f6a5b3c29191f55
Yousaf Nabi <yousafn@gmail.com>
2022-04-07 19:43:33 +0100

    ci: initial CI_PROVIDER_TO_TEST=circleci

--- src/slack/test/setup-tests.ts
+++ src/slack/test/setup-tests.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
-const CI_PROVIDER_TO_TEST = process.env.CI_PROVIDER_TO_TEST || "github";
+const CI_PROVIDER_TO_TEST = process.env.CI_PROVIDER_TO_TEST || "circleci";
 if (!process.env.CI) {
   config({ path: "./config.env.test." + CI_PROVIDER_TO_TEST });
 }
