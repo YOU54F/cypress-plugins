e5d32d0ff410c6f9d747089b8e50a31bca3e7a7f
Yousaf Nabi <you@saf.dev>
2024-10-24 21:54:35 +0100

    fix(deps): update cypress-multi-reporter deps

---
+++ cypress-multi-reporters/eslint.config.js
@@ -0,0 +1,52 @@
+'use strict';
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended, // Recommended config applied to all files
+    {
+        languageOptions: {
+            sourceType: "commonjs",
+            ecmaVersion: 2015,
+            globals: {
+                ...globals.node,
+                ...globals.mocha,
+                ...globals.mocha,
+                expect: true
+
+            },
+        },
+        ignores: [
+            ".editorconfig",
+            "artifacts/",
+            "coverage/",
+            "node_modules/",
+            "lcov-*",
+            "xunit*",
+            "eslint.config.js",
+        ],
+
+        rules: {
+            "brace-style": [2, "stroustrup", { allowSingleLine: true }],
+            "no-console": 0,
+            strict: [2],
+            indent: [2, 4],
+
+            semi: ["error"],
+            "prefer-const": ["error"],
+            "no-var": ["error"],
+            "prefer-destructuring": ["error"],
+            "object-shorthand": ["error"],
+            quotes: ["error", "single"],
+            "quote-props": ["error", "as-needed"],
+            "prefer-template": ["error"],
+        },
+        // files: 'tests/**',
+        // env: {
+        //     mocha: true
+        // // },
+        // globals: {
+        //     expect: true
+        // }
+    },
+];
